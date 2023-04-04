8b56353353ba44c01315623f72cd16c2d2b2b0e3
Bhanu Prakash <64515557+bhanu5972@users.noreply.github.com>
2023-04-04 11:17:14 +0530

    initial commit

---
+++ saveandeditform/src/main/java/gov/nic/poc/saveandeditform/SaveandeditformApplication.java
@@ -0,0 +1,13 @@
+package gov.nic.poc.saveandeditform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveandeditformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveandeditformApplication.class, args);
+	}
+
+}
---
+++ saveandeditform/src/test/java/gov/nic/poc/saveandeditform/SaveandeditformApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.nic.poc.saveandeditform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveandeditformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
