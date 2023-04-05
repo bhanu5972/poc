37ddc120bf5dbecb8b562ba056fa410b1f18a7df
BHANU <BHANU@DESKTOP-F372GA8>
2023-04-05 21:04:50 +0530

    testing from spring boot

--- saveandeditform/src/main/java/gov/nic/poc/saveandeditform/SaveandeditformApplication.java
+++ saveandeditform/src/main/java/gov/nic/poc/saveandeditform/SaveandeditformApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SaveandeditformApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SaveandeditformApplication.class, args);
 	}
 
